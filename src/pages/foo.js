cd79c37d9d23396ed3870231ab68a78d27ca6f32
Łukasz Zieliński <lukasz.zielinski.kontakt@gmail.com>
2020-09-25 13:41:30 +0200

    Adding linking between pages.

--- src/pages/foo.js
+++ src/pages/foo.js
@@ -1,5 +1,13 @@
 import React from "react"
+import { Link } from "gatsby"
 
 export default function Foo() {
-  return <div>Foo page!</div>
+  return (
+    <>
+      <div>Foo page!</div>
+      <button>
+        <Link to="/">Go to index</Link>
+      </button>
+    </>
+  )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,13 @@
 import React from "react"
+import { Link } from "gatsby"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <>
+      <div>Hello world!</div>
+      <button>
+        <Link to="/foo">Go to foo page page</Link>
+      </button>
+    </>
+  )
 }
