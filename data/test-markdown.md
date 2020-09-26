c0329c9bf3e9f0410643f16a61c35f4dba698c90
Łukasz Zieliński <lukasz.zielinski.kontakt@gmail.com>
2020-09-26 20:55:36 +0200

    Adding foo page content genereted based on markdown files.

---
+++ data/test-markdown.md
@@ -0,0 +1,18 @@
+---
+title: "Testowy plik markdown"
+date: "2020-09-25"
+---
+
+## Hej!
+
+To testowy plik **mardown**.
+Dzięki pluginowi `gatsby-transformer-remark` jego zawartość zostanie zamieniona w html.
+
+ - Lista
+ - testowa
+
+~~Nie aktualne~~
+
+![Photo by Charles Deluvio](https://images.unsplash.com/photo-1530041539828-114de669390e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80)
+
+*Photo by Charles Deluvio from [https://unsplash.com](https://unsplash.com/photos/lJJlaUWYrPE)*
--- src/pages/foo.js
+++ src/pages/foo.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { graphql } from 'gatsby'
 import Layout from "../components/Layout"
 
 import styled from "styled-components"
@@ -9,12 +10,42 @@ const StyledContainer = styled.div`
   align-items: center;
 `
 
-export default function Foo() {
+export default function Foo({ data }) {
+  console.log(data);
   return (
     <Layout>
       <StyledContainer>
         <h1>Foo page!</h1>
+        {data.allMarkdownRemark.edges.map(({ node }) => (
+          <div key={node.id}>
+            <h3>
+              {node.frontmatter.title}{" "}
+              <span>
+                — {node.frontmatter.date}
+              </span>
+            </h3>
+            <div dangerouslySetInnerHTML={{ __html: node.html}} />
+            <hr />
+          </div>
+        ))}
       </StyledContainer>
     </Layout>
   )
 }
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(sort: {order: ASC, fields: frontmatter___date}) {
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD.MM.YYYY")
+          }
+          html
+        }
+      }
+    }
+  }
+`
