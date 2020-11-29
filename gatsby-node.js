516e9f865e1868490071d3f1b8c3eee8b2e73d5f
Łukasz Zieliński <lukasz.zielinski.kontakt@gmail.com>
2020-11-29 13:26:46 +0100

    Removing old post markdowns. Adding Netlify CMS to project.

--- gatsby-node.js
+++ gatsby-node.js
@@ -8,7 +8,7 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
     createNodeField({
       node,
       name: `slug`,
-      value: `/blog${slug}`,
+      value: `${slug}`,
     })
   }
 }
@@ -28,7 +28,7 @@ exports.createPages = async ({ graphql, actions }) => {
       }
     }
   `)
-  
+
   result.data.allMarkdownRemark.edges.forEach(({ node }) => {
     createPage({
       path: node.fields.slug,
